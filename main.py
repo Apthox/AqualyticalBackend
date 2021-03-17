f2c2c0ccd88ee2014a1c35087190cf9ca28ac042
Apthox <kevin@apthos.net>
2021-03-16 21:01:35 -0700

    added cors

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from flask import Flask
 import json
 import os
 import process
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def getData(filename):
     with open(filename) as file:
         data = json.load(file)
         return data
 
+# todo: add call for requesting available datasets
+
+# todo: add parameter for selecting dataset
 @app.route('/data')
 def dataEndpoint():
     data = getData('./data/V3136.json')
@@ -17,7 +22,7 @@ def dataEndpoint():
     # Preprocess
     data = process.Preprocess(data)
 
-    return data
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run()
