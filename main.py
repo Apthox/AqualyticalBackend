dff94009b15d6d428d8e0a569e6abc51664405ac
Eduardo445 <esoto-rodriguez@csumb.edu>
2021-04-12 17:52:21 -0700

    Add starter code for POST request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import json
 import os
 import process
@@ -8,21 +9,30 @@ app = Flask(__name__)
 CORS(app)
 
 def getData(filename):
-    with open(filename) as file:
-        data = json.load(file)
-        return data
+  with open(filename) as file:
+    data = json.load(file)
+    return data
 
 # todo: add call for requesting available datasets
 
 # todo: add parameter for selecting dataset
 @app.route('/data')
 def dataEndpoint():
-    data = getData('./data/V3136.json')
+  data = getData('./data/V3136.json')
 
-    # Preprocess
-    data = process.Preprocess(data)
+  # Preprocess
+  data = process.Preprocess(data)
 
-    return json.dumps(data)
+  return json.dumps(data)
+
+# @Shawn TODO: complete this functional POST request to update the data
+# Receiving object { varsConceptID, varsConceptName } 
+@app.route('/update', methods=["POST"])
+def updateTrack():
+  update_data = request.get_json()
+  print(update_data)
+  return "pass"
+
 
 if __name__ == '__main__':
     app.run()
